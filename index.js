1868c4890e32b29d7f37d51919b196e0dddb7645
MunnaAhmed130 <munnaahmed2025@gmail.com>
2022-01-27 15:22:56 +0600

    environment

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000; 
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.dgg2e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
